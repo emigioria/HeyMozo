b545a05983bd57d2e96d35f8b321fbd8edc35ff9
morettilucas <moretti.lucas@hotmail.com>
2017-01-23 01:57:18 -0300

    Comenzada funcionalidad: Lector QR

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/del2016/heymozo/MainActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/del2016/heymozo/MainActivity.java
@@ -1,18 +1,63 @@
 package ar.edu.utn.frsf.isi.dam.del2016.heymozo;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
+
+import ar.edu.utn.frsf.isi.dam.del2016.heymozo.barcode.BarcodeCaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnVerMapa;
+    Button btnVerCarta;
+    TextView resultadoTextView;
+
+    private static final int BARCODE_READER_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnVerMapa = (Button) findViewById(R.id.btn_ver_mapa);
+        btnVerCarta = (Button) findViewById(R.id.btn_ver_carta);
+        resultadoTextView = (TextView) findViewById(R.id.resultado_textview);
 
-        Intent intent = new Intent(this, MapsActivity.class);
-	    startActivity(intent);
+        btnVerMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnVerCarta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
+                startActivityForResult(intent, BARCODE_READER_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BARCODE_READER_REQUEST_CODE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    Point[] p = barcode.cornerPoints;
+                    resultadoTextView.setText(barcode.displayValue);
+                } else resultadoTextView.setText(getString(R.string.codigo_no_escaneado));
+            }
+        }
+        else super.onActivityResult(requestCode, resultCode, data);
     }
 }
