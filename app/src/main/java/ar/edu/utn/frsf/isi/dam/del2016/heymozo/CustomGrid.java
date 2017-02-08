b54686551a2abf2588658d122ad5fe2d1a3dea02
emigioria <emigioria@hotmail.com>
2017-02-08 15:48:23 -0300

    Quitado warning

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/del2016/heymozo/CustomGrid.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/del2016/heymozo/CustomGrid.java
@@ -7,49 +7,45 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
- 
-public class CustomGrid extends BaseAdapter{
-	private Context mContext;
-	private final int[] text;
-    private final int[] Imageid;
 
-	public CustomGrid(Context c, int[] text,int[] Imageid ) {
-            mContext = c;
-            this.Imageid = Imageid;
-            this.text = text;
-        }
- 
+class CustomGrid extends BaseAdapter {
+
+    private final int[] text;
+    private final int[] Imageid;
+    private LayoutInflater inflater;
+
+    CustomGrid(Context c, int[] text, int[] Imageid) {
+        this.Imageid = Imageid;
+        this.text = text;
+        inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        c.getDrawable(1);
+    }
+
     @Override
     public int getCount() {
         return text.length;
     }
- 
+
     @Override
     public Object getItem(int position) {
         return null;
     }
- 
+
     @Override
     public long getItemId(int position) {
         return 0;
     }
- 
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View grid;
-        LayoutInflater inflater = (LayoutInflater) mContext
-            .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
- 
         if (convertView == null) {
-			grid = inflater.inflate(R.layout.item_main, null);
-	        TextView textViewGrid = (TextView) grid.findViewById(R.id.textViewGrid);
-            ImageView imageViewGrid = (ImageView)grid.findViewById(R.id.imageViewGrid);
-            textViewGrid.setText(text[position]);
-            imageViewGrid.setImageResource(Imageid[position]);
-        } else {
-            grid = convertView;
+            convertView = inflater.inflate(R.layout.item_main, parent, false);
         }
+        TextView textViewGrid = (TextView) convertView.findViewById(R.id.textViewGrid);
+        ImageView imageViewGrid = (ImageView) convertView.findViewById(R.id.imageViewGrid);
+        textViewGrid.setText(text[position]);
+        imageViewGrid.setImageResource(Imageid[position]);
 
-        return grid;
+        return convertView;
     }
 }
